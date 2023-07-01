4ebc5a3cd6299d71cab211238499e57181438416
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-07-01 16:31:00 +0800

    getters and setters method done

--- Pet.py
+++ Pet.py
@@ -12,18 +12,35 @@ class Pet:
         self.__age = age
     
 # It should also have the following methods:
-    # • set_name()
+    # set_name()
+    def set_name(self, name):
     # This method assigns a value to the _ _name field.
-    # • set_animal_type()
+        self.__name = name
+
+    # set_animal_type()
+    def set_animal_type(self, animal_type):
     # This method assigns a value to the _ _animal_type field.
+        self.__animal_type = animal_type
+    
     # • set_age()
+    def set_age(self, age):
     # This method assigns a value to the _ _age field.
-    # • get_name()
+        self.__age = age
+        
+    # get_name()
+    def get_name(self):
     # This method returns the value of the _ _ name field.
-    # • get_animal_type()
+        return self.__name
+    
+    # get_animal_type()
+    def get_animal_type(self):
     # This method returns the value of the _ _animal_type field.
-    # • get_age()
+        return self.__animal_type
+    
+    # get_age()
+    def get_age(self):
     # This method returns the value of the _ _age field.
+        return self.__age
     # Once you have written the class, write a program that creates an object of the class 
     # and prompts the user to enter the name, type, and age of his or her pet. This data should be stored as the object’s attributes. 
     # Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
