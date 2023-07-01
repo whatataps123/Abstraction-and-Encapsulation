720a8588cdbaab5c81a5176c04e556745c5bc1df
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-07-01 15:14:43 +0800

    added class Car and __init__ method with attributes

--- Car.py
+++ Car.py
@@ -1,12 +1,14 @@
 # Joshua Lemuel Z. Centina BS CPE 1-4
 # pseudocode
 # Write a class named Car that has the following data attributes:
-# • _ _year_model (for the car’s year model)
-# • _ _make (for the make of the car)
-# • _ _speed (for the car’s current speed)
-# The Car class should have an _ _init_ _ method that accepts the car’s year model and make as arguments.
-# These values should be assigned to the object’s _ _year_model and _ _make data attributes. 
-#It should also assign 0 to the _ _speed data attribute.
+class Car:
+    def __init__(self, year_model, make, speed=0):
+        # _ _year_model (for the car’s year model)
+        self.__year_model = year_model
+        # _ _make (for the make of the car)
+        self.__make = make
+        # _ _speed (for the car’s current speed)
+        self.__speed = speed
 # The class should also have the following methods:
 # • accelerate()
 # The accelerate method should add 5 to the speed data attribute each time it is called.
