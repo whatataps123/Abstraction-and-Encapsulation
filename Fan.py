2c233f72e01512006ad4d92f9e0d2d4a7fa27007
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-07-01 11:41:00 +0800

    fan pseudocode

---
+++ Fan.py
@@ -0,0 +1,13 @@
+# Joshua Lemuel Z. Centina BS CPE 1-4
+# pseudocode
+# (The Fan class).  Design a class named Fan to represent a fan. The class contains:
+# Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to denote the fan speed.
+# A private int data field named speed that specifies the speed of the fan.
+# A private bool data field named on that specifies whether the fan is on (the default is False).
+# A private float data field named radius that specifies the radius of the fan.
+# A private string data field named color that specifies the color of the fan.
+# The accessor(getters)  and mutator(setters)  methods for all four data fields.
+# A constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False).
+# Write a test program named TestFan that creates two Fan objects. For the first object, assign the maximum speed, radius 10, color yellow, and turn it on. 
+# Assign medium speed, radius 5, color blue, and turn it off for the second object. 
+# Display each object’s speed, radius, color, and on properties.
